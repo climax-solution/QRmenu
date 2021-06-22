a5d959ebddfda4d86ccf38f2a8259b3f7504ed7f
climax-solution <blocklimax@gmail.com>
2021-06-23 08:59:52 +1000

    complete user package

--- src/routes/userspackage/index.js
+++ src/routes/userspackage/index.js
@@ -13,14 +13,17 @@
  // intl messages
  import IntlMessages from 'Util/IntlMessages';
  
- import { TextField,  Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, InputLabel, Select, MenuItem, FormControl, Checkbox, FormControlLabel, FormGroup, FormLabel,FormHelperText} from '@material-ui/core';
+ import { TextField,  Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, InputLabel, Select, MenuItem, FormControl, Checkbox, FormControlLabel, FormGroup,FormHelperText,Input} from '@material-ui/core';
  
+ import 'Assets/css/userpackage.css';
+
  export default class UsersPackage extends Component {
     state = {
         monthlyPlan: true,
         premiumPlan: 300,
         enterprisePlan: 590,
-        open: false
+        open: false,
+        modalTitle: 'Add Package'
     }
  
     // on plan change
@@ -32,8 +35,9 @@
             this.setState({ businessPlan: 350, enterprisePlan: 700 });
         }
     }
-    handleClickOpen = () => {
-        this.setState({ open: true });
+    handleClickOpen = (arg = false) => {
+        let text = arg == true ? 'Add Package' : 'Edit Package';
+        this.setState({ open: true, modalTitle: text });
     };
   
     handleClose = () => {
@@ -62,7 +66,7 @@
                 <div className="row">
                     <div className="col-md-4"></div>
                     <div className="col-md-4">
-                        <Button color="success" className='btn-block btn-lg' onClick={this.handleClickOpen}>
+                        <Button color="success" className='btn-block btn-lg' onClick={()=>this.handleClickOpen(true)}>
                             <IntlMessages id="Add New" />
                         </Button>
                     </div>
@@ -77,7 +81,7 @@
                             description="Secure file sharing and collaboration. Ideal for small teams."
                             price="widgets.free"
                             users={1}
-                            handleClickOpen = {this.handleClickOpen}
+                            handleClickOpen = {()=>this.handleClickOpen(false)}
                             features={[
                                 'Velkommen side',
                                 'Meny (50 items)',
@@ -98,7 +102,7 @@
                             description="Secure file sharing and collaboration. Ideal for small teams."
                             price={this.state.premiumPlan}
                             users={1}
-                            handleClickOpen = {this.handleClickOpen}
+                            handleClickOpen = {()=>this.handleClickOpen(false)}
                             features={[
                                 'Velkommen side',
                                 'Meny (Unlimited items)',
@@ -118,88 +122,102 @@
                     <DialogTitle id="form-dialog-title">Package Management</DialogTitle>
                     <DialogContent>
                         <DialogContentText>
-                            Edit your package
+                            {this.state.modalTitle}
                         </DialogContentText>
-                        <TextField margin="dense" id="name" label="Package name" type="text" className="width-50" fullWidth/>
-                        <TextField margin="dense" id="slug" label="Slug" type="text" width="200px" className="width-50" fullWidth/>
-                        <TextField margin="dense" id="email" label="Email Address" type="email" fullWidth/>
-                        <FormControl fullWidth>
-                            <InputLabel htmlFor="orderLimit">Order Limit</InputLabel>
-                            <Select labelId="label" id="orderLimit">
-                                <MenuItem value="">Select Amount</MenuItem>
-                                <MenuItem value="-1">Unlimit</MenuItem>
-                                <MenuItem value="10">10</MenuItem>
-                                <MenuItem value="15">15</MenuItem>
-                                <MenuItem value="20">20</MenuItem>
-                                <MenuItem value="30">30</MenuItem>
-                                <MenuItem value="50">50</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <FormControl fullWidth>
-                            <InputLabel htmlFor="itemLimit">Item Limit</InputLabel>
-                            <Select labelId="label" id="itemLimit">
-                             <MenuItem value="">Select Amount</MenuItem>
-                             <MenuItem value="-1">Unlimit</MenuItem>
-                             <MenuItem value="10">10</MenuItem>
-                                <MenuItem value="15">15</MenuItem>
-                                <MenuItem value="20">20</MenuItem>
-                                <MenuItem value="30">30</MenuItem>
-                                <MenuItem value="40">40</MenuItem>
-                                <MenuItem value="50">50</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <FormControl component="fieldset">
-                            <FormGroup>
-                                <FormControlLabel
-                                    control={<Checkbox name="velkom" />}
-                                    label="Velkommen Side"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox name="meny" />}
-                                    label="Meny"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox name="pakker" />}
-                                    label="Pakker"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox name="spesial" />}
-                                    label="Spesialiteter"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox name="qrcode" />}
-                                    label="QRCode"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox name="whatsbest" />}
-                                    label="Whatsapp Bestilling"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox name="onlinebest" />}
-                                    label="Online Bestilling"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox name="reserve" />}
-                                    label="Reservajon"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox name="contacter" />}
-                                    label="Contacter"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox name="digitalbetal" />}
-                                    label="Digital Betaling"
-                                />
-                            </FormGroup>
-                        </FormControl>
-                        
+                        <div className="row">                        
+                            <div className="col-md-6" >
+                                <TextField margin="dense" id="name" label="Package name" type="text" fullWidth/>
+                                <TextField margin="dense" id="slug" label="Slug" type="text" fullWidth/>
+                                <FormControl margin="dense" fullWidth>
+                                    <InputLabel htmlFor="packagetype">Package Type</InputLabel>
+                                    <Select id="packagetype">
+                                        <MenuItem value="">Select</MenuItem>
+                                        <MenuItem value="free">free</MenuItem>
+                                        <MenuItem value="month">monthly</MenuItem>
+                                        <MenuItem value="year">yearly</MenuItem>
+                                    </Select>
+                                </FormControl>
+                                <FormControl margin="dense"  fullWidth>
+                                    <InputLabel htmlFor="orderLimit">Order Limit</InputLabel>
+                                    <Select id="orderLimit">
+                                        <MenuItem value="">Select Amount</MenuItem>
+                                        <MenuItem value="-1">Unlimit</MenuItem>
+                                        <MenuItem value="10">10</MenuItem>
+                                        <MenuItem value="15">15</MenuItem>
+                                        <MenuItem value="20">20</MenuItem>
+                                        <MenuItem value="30">30</MenuItem>
+                                        <MenuItem value="50">50</MenuItem>
+                                    </Select>
+                                </FormControl>
+                                <TextField margin="dense" id="price" label="Price" type="text" fullWidth/>
+                                <FormControl margin="dense"  fullWidth>
+                                    <InputLabel htmlFor="itemLimit">Item Limit</InputLabel>
+                                    <Select id="itemLimit">
+                                    <MenuItem value="">Select Amount</MenuItem>
+                                    <MenuItem value="-1">Unlimit</MenuItem>
+                                    <MenuItem value="10">10</MenuItem>
+                                        <MenuItem value="15">15</MenuItem>
+                                        <MenuItem value="20">20</MenuItem>
+                                        <MenuItem value="30">30</MenuItem>
+                                        <MenuItem value="40">40</MenuItem>
+                                        <MenuItem value="50">50</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
+                            <div className="col-md-6">
+                                <FormControl component="fieldset" className="pull-right">
+                                    <FormGroup>
+                                        <FormControlLabel
+                                            control={<Checkbox name="velkom" />}
+                                            label="Velkommen Side"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox name="meny" />}
+                                            label="Meny"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox name="pakker" />}
+                                            label="Pakker"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox name="spesial" />}
+                                            label="Spesialiteter"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox name="qrcode" />}
+                                            label="QRCode"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox name="whatsbest" />}
+                                            label="Whatsapp Bestilling"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox name="onlinebest" />}
+                                            label="Online Bestilling"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox name="reserve" />}
+                                            label="Reservajon"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox name="contacter" />}
+                                            label="Contacter"
+                                        />
+                                        <FormControlLabel
+                                            control={<Checkbox name="digitalbetal" />}
+                                            label="Digital Betaling"
+                                        />
+                                    </FormGroup>
+                                </FormControl>
+                            </div>
+                        </div>
                     </DialogContent>
                     <DialogActions>
                         <Button variant="contained" onClick={this.handleClose} color="primary" className="text-white">
                             Cancel
                         </Button>
                         <Button variant="contained" onClick={this.handleClose} className="btn-info text-white">
-                            Subscribe
+                            Submit
                         </Button>
                     </DialogActions>
                 </Dialog>
