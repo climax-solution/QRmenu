d6710db2ae5d8cd63262cbcd05e947464feb77c6
climax-solution <blocklimax@gmail.com>
2021-06-25 12:49:48 +1000

    add attribute(style, setable)

--- src/components/RctCollapsibleCard/RctCollapsibleCard.js
+++ src/components/RctCollapsibleCard/RctCollapsibleCard.js
@@ -7,13 +7,14 @@ import classnames from 'classnames';
 
 // rct section loader
 import RctSectionLoader from '../RctSectionLoader/RctSectionLoader';
-
+import Switch from 'react-toggle-switch';
 class RctCollapsibleCard extends Component {
 
     state = {
         reload: false,
         collapse: true,
-        close: false
+        close: false,
+        status: true,
     }
 
     onCollapse() {
@@ -32,20 +33,26 @@ class RctCollapsibleCard extends Component {
         this.setState({ close: true });
     }
 
+    onSetting() {
+        this.setState({
+            status: !this.state.status
+        });
+    }
     render() {
         const { close, reload, collapse } = this.state;
-        const { children, collapsible, closeable, reloadable, heading, fullBlock, colClasses, customClasses, headingCustomClasses, contentCustomClasses, badge } = this.props;
+        const { children, collapsible, closeable, reloadable, heading, fullBlock, colClasses, customClasses, headingCustomClasses, contentCustomClasses, badge, setable, customStyle } = this.props;
         return (
             <div className={classnames(colClasses ? colClasses : '', { 'd-block': !collapse })}>
-                <div className={classnames(`rct-block ${customClasses ? customClasses : ''}`, { 'd-none': close })}>
+                <div className={classnames(`rct-block ${customClasses ? customClasses : ''}`, { 'd-none': close })} style={customStyle}>
                     {heading &&
                         <div className={`rct-block-title ${headingCustomClasses ? headingCustomClasses : ''}`}>
                             <h4>{heading} {badge && <Badge className="p-1 ml-10" color={badge.class}>{badge.name}</Badge>}</h4>
-                            {(collapsible || reloadable || closeable) &&
+                            {(collapsible || reloadable || closeable || setable) &&
                                 <div className="contextual-link">
                                     {collapsible && <a href="javascript:void(0)" onClick={() => this.onCollapse()}><i className="ti-minus"></i></a>}
                                     {reloadable && <a href="javascript:void(0)" onClick={() => this.onReload()}><i className="ti-reload"></i></a>}
                                     {closeable && <a href="javascript:void(0)" onClick={() => this.onCloseSection()}><i className="ti-close"></i></a>}
+                                    {setable && <Switch onClick={()=>this.onSetting()} on={this.state.status}/>}
                                 </div>
                             }
                         </div>
